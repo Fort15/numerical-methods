4289e15506cf25eb194961b32c8739a18f4c9519
Maxim Alexandrov <git.user.MAKS-110606@yandex.ru>
2026-09-14 22:05:39 +0300

    1.2 + 1.3

---
+++ lab1/1.3.py
@@ -0,0 +1,120 @@
+from utils.io_helpers import read_matrix, read_vector, print_vector
+
+
+def norm_matrix(A):
+    """Норма матрицы по строкам"""
+    n = len(A)
+    return max([sum([abs(A[i][j]) for j in range(n)]) for i in range(n)])
+
+
+def simple_iterations(alpha, beta, eps=0.01):
+    max_iter = 10000
+    n = len(beta)
+    x = beta[:]
+
+    for k in range(1, max_iter + 1):
+        x_new = [0.0] * n
+        for i in range(n):
+            s = beta[i]
+            for j in range(n):
+                if i != j:
+                    s += alpha[i][j] * x[j]
+            x_new[i] = s
+
+        diff = max([abs(x_new[i] - x[i]) for i in range(n)])
+        x = x_new
+
+        if diff < eps:
+            return x, k
+
+    return x, max_iter
+
+
+def zeidel(alpha, beta, eps=0.01):
+    max_iter = 10000
+    n = len(beta)
+    x = beta[:]
+
+    for k in range(1, max_iter + 1):
+        x_old = x[:]
+        for i in range(n):
+            s = beta[i]
+            for j in range(n):
+                if i != j:
+                    s += alpha[i][j] * x[j]
+            x[i] = s
+
+        diff = max([abs(x[i] - x_old[i]) for i in range(n)])
+
+        if diff < eps:
+            return x, k
+
+    return x, max_iter
+
+
+def create_alpha_beta(A, b):
+    """
+    Ax=b -> x = alpha * x + beta
+    """
+    n = len(A)
+
+    for i in range(n):
+        if abs(A[i][i]) < 1e-15:            # нулевой диагональный элемент -> перестановка строк
+            for k in range(i + 1, n):
+                if abs(A[k][i]) > 1e-15:
+                    A[i], A[k] = A[k], A[i]
+                    b[i], b[k] = b[k], b[i]
+                    break
+            raise ValueError(f'Нулевой столбец {i + 1}')
+
+    alpha = [[0.0] * n for _ in range(n)]
+    beta = [0.0] * n
+    for i in range(n):
+        beta[i] = b[i] / A[i][i]
+        for j in range(n):
+            if i != j:
+                alpha[i][j] = -A[i][j] / A[i][i]
+
+    return alpha, beta
+
+
+def main():
+    A = read_matrix('data/1.3_A.txt')
+    b = read_vector('data/1.3_b.txt')
+    n = len(A)
+
+    eps = 0.01
+    print(f'Заданная точность eps = {eps}')
+    print()
+
+    alpha, beta = create_alpha_beta(A, b)
+
+    if norm_matrix(alpha) < 1:
+        print(f'||alpha|| < 1, условие сходимости выполнено')
+    else:
+        print(f'||alpha|| >= 1, сходимость не гаранитрована')
+    print()
+
+    x_simp_it, count_simp_it = simple_iterations(alpha, beta, eps=eps)
+    print(f'Метод простой итерации:')
+    print(f'Число итераций: {count_simp_it}')
+    print_vector(x_simp_it, title='Решение x:')
+    print()
+
+    x_zeid, count_zeid_it = zeidel(alpha, beta, eps=eps)
+    print(f'Метод Зейделя:')
+    print(f'Число итераций: {count_zeid_it}')
+    print_vector(x_zeid, title='Решение x:')
+    print()
+
+    print('Сравнение методов:')
+    if x_zeid < x_simp_it:
+        print(f'Зейдель быстрее в {count_simp_it / count_zeid_it:.2f} раз')
+    elif x_zeid > x_simp_it:
+        print(f'Простые итерации быстрее в {count_zeid_it / count_simp_it:.2f} раз')
+    else:
+        print('Одинаковое число итераций')
+
+
+if __name__ == '__main__':
+    main()
---
+++ lab1/1_2.py
@@ -0,0 +1,67 @@
+from utils.io_helpers import print_vector
+
+
+def read_tridiagonal_matrix(path):
+    """
+    Читает трёхдиагональную матрицу из файла
+    Формат
+    n
+    a1 ... an (поддиагональ, a1 = 0)
+    b1 ... bn (главная диагональ)
+    c1 ... cn (наддиагональ, cn = 0)
+    d1 ... dn (правая часть)
+    Возвращает (a, b, c, d, n)
+    """
+    with open(path, 'r', encoding='utf-8') as f:
+        tokens = f.read().split()
+
+    n = int(tokens[0])
+    if len(tokens) != 1 + 4 * n:
+        raise ValueError(f'Введено неверное количество чисел: {len(tokens)}')
+
+    a = [float(x) for x in tokens[1:n + 1]]
+    b = [float(x) for x in tokens[n + 1:2 * n + 1]]
+    c = [float(x) for x in tokens[2 * n + 1:3 * n + 1]]
+    d = [float(x) for x in tokens[3 * n + 1:4 * n + 1]]
+
+    return a, b, c, d, n
+
+
+def runoff_coef(a, b, c, d, n):
+    P = [-c[0] / b[0]]
+    Q = [d[0] / b[0]]
+
+    for i in range(1, n):
+        Pi = -c[i] / (b[i] + a[i] * P[i - 1])
+        Qi = (d[i] - a[i] * Q[i - 1]) / (b[i] + a[i] * P[i - 1])
+
+        P.append(Pi)
+        Q.append(Qi)
+
+    return P, Q
+
+
+def reverse_move(P, Q, n):
+    x = [0.0] * n
+    x[-1] = Q[-1]
+    for i in range(n-2 , -1, -1):
+        x[i] = P[i] * x[i + 1] + Q[i]
+
+    return x
+
+
+def main():
+    a, b, c, d, n = read_tridiagonal_matrix('data/1.2.txt')
+
+    P, Q = runoff_coef(a, b, c, d, n)
+
+    for i in range(n):
+        print(f'P{i + 1} = {P[i]:.4f}\nQ{i + 1} = {Q[i]:.4f}')
+    print()
+
+    x = reverse_move(P, Q, n)
+    print_vector(x, title='Итоговое решение СЛАУ:')
+
+
+if __name__ == '__main__':
+    main()
